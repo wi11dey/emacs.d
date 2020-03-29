5bba5c93b4ec372d08bc63a1c0ea765a0bfa8d72
Will Dey <52264243+will-dey@users.noreply.github.com>
2020-03-28 23:25:13 -0400

    Switch straight-init.el to lexical binding

--- straight-init.el
+++ straight-init.el
@@ -1,4 +1,5 @@
-;; Initialize straight.el by evaluating all `eval-whe-compile' code in init.el:
+;;; straight-init.el --- Initialize straight.el by evaluating all `eval-whe-compile' code in init.el -*- lexical-binding: t -*-
+
 (with-temp-buffer
   (insert-file-contents (expand-file-name "init.el" user-emacs-directory))
   (goto-char (point-min))
@@ -6,3 +7,5 @@
       (while t
 	(macroexpand-all (read (current-buffer))))
     (end-of-file nil)))
+
+;;; straight-init.el ends here
