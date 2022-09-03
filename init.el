ceee7ff304368947d3de02171ea6505576c31715
Will Dey <will_dey@college.harvard.edu>
2022-09-03 17:25:33 -0400

    Use README.org as commentary

--- init.el
+++ init.el
@@ -22,9 +22,7 @@
 
 ;;; Commentary:
 
-;; Using my library [[https://github.com/wi11dey/p-ckage][p@ckage]] rather than `use-package' for package organization.
-
-;; Starts in 0.7s on good days, 2s on bad
+;; See README.org.
 
 ;;; Code:
 
