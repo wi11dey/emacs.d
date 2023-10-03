81bd076f93d3a1132696d0ed94c19f5d1241b509
Will Dey <wdey@palantir.com>
2023-10-03 10:42:34 -0400

    extra native-comp protection

--- early-init.el
+++ early-init.el
@@ -1,4 +1,6 @@
-(when (fboundp 'startup-redirect-eln-cache)
+(when (and (fboundp 'startup-redirect-eln-cache)
+	   ;; Sometimes `startup-redirect-eln-cache' is defined but fails with "Symbol's value as variable is void: native-comp-eln-load-path":
+	   (boundp 'native-comp-eln-load-path))
   (startup-redirect-eln-cache
    (convert-standard-filename
     (expand-file-name  "var/eln-cache/" user-emacs-directory))))
