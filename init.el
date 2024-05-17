b1cbd677273d468949818ec757bb8e7081436f25
Will Dey <wdey@palantir.com>
2024-05-17 13:15:07 +0100

    Move `read-hide-char' to emaχ theme

--- init.el
+++ init.el
@@ -358,9 +358,6 @@ Optional argument FILE-OVERRIDE is a string to be passed as the FILE parameter t
 
 ;;; Emacs
 (p@ckage emacs
-  ;;;; Password character
-  (setq read-hide-char ?•)
-
   ;;;; Graphical display
   ;; xdisp
   (setq x-underline-at-descent-line t ; Lines up underline with `telephone-line' separators.
