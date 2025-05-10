880aabc31d614f84de582db45052d057c29aec0f
Will Dey <wdey@palantir.com>
2025-05-10 17:40:08 +0100

    org-modern is already called by emaχ

--- init.el
+++ init.el
@@ -1613,14 +1613,6 @@ See also Info node `(eshell)Top'."
     ;;;;;; Disable
     (setq $-cache-persistent nil))
 
-  ;;;; Modern
-  (p@ckage $-modern
-    ;;;;; Build
-    ~(straight-use-package '$)
-
-    ;; TODO setup
-    )
-
   ;;;; Remote
   ;; It is not safe for files to allow themselves to get remote images:
   (put '$-display-remote-inline-images 'safe-local-variable #'ignore)
